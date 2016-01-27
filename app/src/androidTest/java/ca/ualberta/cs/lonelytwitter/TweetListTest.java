6af0501430636112b73ab22f030c74589f78cb5d
stuarthoye <hoye@ualberta.ca>
2016-01-26 17:37:44 -0700

    Add code to be tested.

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -11,23 +11,29 @@ public class TweetListTest extends ActivityInstrumentationTestCase2 {
     }
 
     public void testAddTweet() {
+        //  Setup
         TweetList tweets = new TweetList();
         Tweet tweet = new NormalTweet("Hello II");
 
+        // Add a tweet
         tweets.add(tweet);
 
+        // Test that the tweet is present.
         assertTrue(tweets.hasTweet(tweet));
     }
 
     public void testHasTweet() {
+        //  Setup
         TweetList tweets = new TweetList();
         Tweet tweet = new NormalTweet("Hello");
 
+        //  Test that the list is empty to begin
         assertFalse(tweets.hasTweet(tweet));
 
+        //  Add a Tweet
         tweets.add(tweet);
-        tweets.hasTweet(tweet);
 
+        //  Test that the test returns true for the tweet.
         assertTrue(tweets.hasTweet(tweet));
     }
 }
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -9,11 +9,11 @@ public class TweetList {
     private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 
     public void add(Tweet tweet) {
-
+        tweets.add(tweet);
     }
 
     public boolean hasTweet(Tweet tweet) {
-        return Boolean.FALSE;               // TEMPORARY
+        return tweets.contains(tweet);
     }
 
 
