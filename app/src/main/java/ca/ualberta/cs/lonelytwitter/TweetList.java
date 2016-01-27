3c4d02a5593380632f33a5c530ffcce4b2400eb2
stuarthoye <hoye@ualberta.ca>
2016-01-26 17:18:47 -0700

    Add empty functions to TweetList

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetList.java
@@ -1,7 +1,20 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.ArrayList;
+
 /**
  * Created by hoye on 1/26/16.
  */
 public class TweetList {
+    private ArrayList<Tweet> tweets = new ArrayList<Tweet>();
+
+    public void add(Tweet tweet) {
+
+    }
+
+    public boolean hasTweet(Tweet tweet) {
+        return Boolean.FALSE;               // TEMPORARY
+    }
+
+
 }
