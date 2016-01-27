27082a70bc8e454e62212f4cd404a2502fd8a070
stuarthoye <hoye@ualberta.ca>
2016-01-26 17:22:31 -0700

    Add fail test

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -1,8 +1,17 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import android.test.ActivityInstrumentationTestCase2;
+
 /**
  * Created by hoye on 1/26/16.
  */
-public class TweetListTest {
+public class TweetListTest extends ActivityInstrumentationTestCase2{
+    public TweetListTestest(){
+        super(LonelyTwitterActivity.class);
+    }
+
+    public void testAddTweet() {
+        assertTrue(Boolean.FALSE);
+    }
 
 }
